504f06c9e3c4590deb2f28d2097be005315e2981
zhongjunhaoz <2236241424@qq.com>
2019-05-29 23:05:17 +0800

    汽车小程序-宁皓网

---
+++ landRover/pages/index/index.js
@@ -0,0 +1,26 @@
+//index.js
+//获取应用实例
+const app = getApp()
+const db = require('../../asset/db.js')
+Page({
+  data: {
+    slides: db.slides
+  },
+  //事件处理函数
+  bindViewTap: function () {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+
+  },
+  getUserInfo: function (e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
