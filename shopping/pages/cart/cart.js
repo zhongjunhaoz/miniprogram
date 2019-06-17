c3e2b19247aca6599596a92d753d78b388efde29
zhongjunhaoz <2236241424@qq.com>
2019-06-17 19:24:26 +0800

    模块封装 购物小程序

---
+++ shopping/pages/cart/cart.js
@@ -0,0 +1,141 @@
+// pages/cart/cart.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    setTimeout(() => {
+      this.setData({
+        hasList: true,
+        carts: [
+          { id: 1, title: '新鲜芹菜 半斤', image: '/image/s5.png', num: 4, price: 0.01, selected: true },
+          { id: 2, title: '素米 500g', image: '/image/s6.png', num: 1, price: 0.03, selected: true }
+        ]
+
+      })
+      this.getTotalPrice()
+    },1000)
+  },
+  selectAll() {
+    let selectAllStatus = this.data.selectAllStatus
+    selectAllStatus = !selectAllStatus
+    let carts = this.data.carts
+    for(let i = 0; i < carts.length; i++) {
+      carts[i].selected = selectAllStatus
+    }
+    this.setData({
+      selectAllStatus: selectAllStatus,
+      carts: carts
+    })
+  },
+  getTotalPrice(){
+    let carts = this.data.carts;
+    let total = 0;
+    for(let i=0;i<carts.length;i++){
+      if(carts[i].selected){
+        total += carts[i].num * carts[i].price
+      }
+    }
+    this.setData({
+      totalPrice:total.toFixed(2)
+    })
+  },
+  minusCount(e){
+    // console.log(e)
+    const index = e.target.dataset.index
+    let carts = this.data.carts
+    let num = carts[index].num
+    if(num<=1){
+      return
+    }
+    num = num -1;
+    carts[index].num = num;
+    this.setData({
+      carts:carts
+    })
+    this.getTotalPrice()
+  },
+  addCount(e){
+    const index = e.target.dataset.index;
+    let carts = this.data.carts;
+    let num = carts[index].num;
+    num = num + 1;
+    carts[index].num = num;
+    this.setData({
+      carts: carts
+    })
+    this.getTotalPrice()
+  },
+  deleteList(e){
+    const index = e.target.dataset.index;
+    let carts = this.data.carts;
+    carts.splice(index,1);//从index开始删，删一个
+    this.setData({
+      carts: carts
+    })
+    //判断一个数组有没有值，一般看长度
+    if (!carts.length){
+      this.setData({
+        hasList:false
+      })
+    } else{
+      this.getTotalPrice()
+    }
+  },
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
